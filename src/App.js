aeaa2e2c2fd847c3af5075158b56ac85e5e1dc1f
Murali-uppalapu <tejauppalapu.com>
2022-12-02 11:49:57 +0530

    firstapp

---
+++ src/App.js
@@ -0,0 +1,40 @@
+/*import ClassComponent from './ClassComponent'
+import ClassComponent2 from './ClassComponent2'
+function App(){
+ return (
+ <section>
+     <p>Hello fellows</p>
+     <h1>This is h1 tag</h1>
+     <ClassComponent/>
+     <ClassComponent2/>
+     <img src="https://www.mixindia.com/wp-content/uploads/2022/07/Ivana-Photos-1.jpg"/>
+   </section>
+   )
+ }
+export default App*/
+
+//
+//import PropsExample from './PropsExample'
+
+//import FunPropExample from './FunPropExample'
+
+//import StateExample from './StateExample'
+
+import Event from "./Event"
+function App(){
+    return(
+        <div>
+          <h1>Events</h1>
+          <Event/>
+        {/*  <StateExample/>
+         <h1>This is about props example</h1>
+        <PropsExample name="angularjs" year="2008" />
+        <PropsExample name="reactjs" year="2013" />
+        <PropsExample name="vuejs" year="2018" /> 
+        <FunPropExample framework="react" number="1"/>*/}
+        </div>
+        )
+}
+
+ export default App
+  
