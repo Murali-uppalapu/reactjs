ba7c39325ada990d691bb989dc9e64f6eb59fbaf
Murali-uppalapu <tejauppalapu.com>
2022-12-05 11:49:40 +0530

    learned abour router method

--- src/App.js
+++ src/App.js
@@ -1,3 +1,36 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom' 
+import Home from './Components/Home'
+import About from './Components/About'
+import Contact from './Components/Contact'
+import Navbar from './Components/Navbar'
+import NotFound from './Components/NotFound'
+
+function App(){
+    return(
+        <div>
+          <BrowserRouter>
+          <Navbar/>
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/about" element={<About/>}/>
+              <Route path="/contact" element={<Contact/>}/>
+              <Route path="*" element={<NotFound/>}/>
+            </Routes>
+          </BrowserRouter>
+         {/* <Event/>
+          <StateExample/>
+         <h1>This is about props example</h1>
+        <PropsExample name="angularjs" year="2008" />
+        <PropsExample name="reactjs" year="2013" />
+        <PropsExample name="vuejs" year="2018" /> 
+        <FunPropExample framework="react" number="1"/>*/}
+        </div>
+        )
+}
+
+ export default App
+  
+
 /*import ClassComponent from './ClassComponent'
 import ClassComponent2 from './ClassComponent2'
 function App(){
@@ -20,21 +53,4 @@ export default App*/
 
 //import StateExample from './StateExample'
 
-import Event from "./Event"
-function App(){
-    return(
-        <div>
-          <h1>Events</h1>
-          <Event/>
-        {/*  <StateExample/>
-         <h1>This is about props example</h1>
-        <PropsExample name="angularjs" year="2008" />
-        <PropsExample name="reactjs" year="2013" />
-        <PropsExample name="vuejs" year="2018" /> 
-        <FunPropExample framework="react" number="1"/>*/}
-        </div>
-        )
-}
-
- export default App
-  
+//import Event from "./Event"
